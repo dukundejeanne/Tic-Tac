586b56524a0c45dd5444b3ef74a8da608fdbefdd
dukundejeanne <dukundem90@gmail.com>
2020-01-31 23:18:32 +0200

    print

--- user_test.py
+++ user_test.py
@@ -3,46 +3,23 @@ import unittest # Importing the unittest module
 
 class TestTicTacToe(unittest.TestCase):
 
-#     '''
-#     Test class that defines test cases for the TicTacToe class behaviours.
+    '''
+    Test class that defines test cases for the TicTacToe class behaviours.
 
-#     Args:
-#         unittest.TestCase: TestCase class that helps in creating test cases
-#     '''
-
-#        # Items up here .......
-
-#     def setUp(self):
-#         '''
-#         Set up method to run before each test cases.
-#         '''
-#         self.new_board = TicTacToe() # create contact object
-
-
-#     def test_init(self):
-#         '''
-#         test_init test case to test if the object is initialized properly
-#         '''
-
-#         self.assertEqual(self.new_board.position,1)
-#         # self.assertEqual(self.new_board.last_name,"Muriuki")
-#         # self.assertEqual(self.new_contact.phone_number,"0712345678")
-#         # self.assertEqual(self.new_contact.email,"james@ms.com")
-
-#     def test_save_board(self):
-#         '''
-#         test_save_contact test case to test if the contact object is saved into
-#          the contact list
-#         '''
-#         self.new_board.save() # saving the new contact
-#         # self.assertEqual(len(Contact.contact_list),1)
-# class NumberTest(unittest.TestCase):
-    
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+     
     board="x"
     board="0"
     def setUp(self): 
+        '''
+        Set up method to run before each test cases.
+        '''
         pass
     
+       # Items up here .......
+   
     def make_board(self):
         return Board()
 
@@ -68,50 +45,10 @@ class TestTicTacToe(unittest.TestCase):
         self.assertEqual( 'x'*3, 'xxx') 
 
 
-    # def setUp(self):
-    #     self.board= self.make_board()
-    # def test_too_few_markers(self):
-    #     with self.assertRaises(InvalidMarkers):
-    #         board = Board("O")
-    # def test_too_many_markers(self):
-    #     with self.assertRaises(InvalidMarkers):
-    #         board= Board("OXY")
-    # def test_duplicate_markers(self):
-    #     with self.assertRaises(InvalidMarkers):
-    #         board = Board("OO")
-
     def test_check_tie(self):
         
         self.assertFalse(self.board.is_full())
-
-    # def test_startgrid_is_empty_and_not_full(self):
-    #     assert(self.board.is_empty())
-    #     self.assertFalse(self.board.is_full())
-    # def test_not_empty_and_not_full_after_play_center(self):
-    #     assert(self.grid.play('center'))
-    #     assert(not self.grid.is_empty())
-  
-    # Returns True if the string is in upper case. 
-  
-    # # Returns TRUE if the string is in uppercase 
-    # # else returns False. 
-    # def test_isupper(self):         
-    #     self.assertTrue('FOO'.isupper()) 
-    #     self.assertFalse('Foo'.isupper()) 
-  
-    # # Returns true if the string is stripped and  
-    # # matches the given output. 
-    # def test_strip(self):         
-    #     s = 'geeksforgeeks'
-    #     self.assertEqual(s.strip('geek'), 'sforgeeks') 
-  
-    # # Returns true if the string splits and matches 
-    # # the given output. 
-    # def test_split(self):         
-    #     s = 'hello world'
-    #     self.assertEqual(s.split(), ['hello', 'world']) 
-    #     with self.assertRaises(TypeError): 
-            # s.split(2) 
+ 
 if __name__ == '__main__':
     unittest.main()
 
