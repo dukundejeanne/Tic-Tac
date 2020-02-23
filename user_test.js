b741a9da7f5dc5bc668fa4f217b5e92cc4da0f5b
dukundejeanne <dukundem90@gmail.com>
2020-02-23 22:00:05 +0200

    js

---
+++ user_test.js
@@ -0,0 +1,13 @@
+// var ul;
+
+// setup(function(){
+//   ul = $('<ul><li class="no-results"></li></ul>');
+// });
+
+// test('constructor', function () {
+//   var l = new Likes(ul);
+//   assert(l);
+// });
+QUnit.test( "hello test", function( assert ) {
+    assert.ok( 1 == "1", "Passed!" );
+  });
